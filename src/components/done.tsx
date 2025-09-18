32a0907e055c6d70aa017b858f67168d080b2294
Zakhe <zakhenet@gmail.com>
2025-09-18 12:54:16 +0200

    tests

--- src/components/done.tsx
+++ src/components/done.tsx
@@ -9,7 +9,7 @@ export default function Done(carID,setPage){
                 COMPLETE
             </div>
             <p className="infoDone fs-4">You have successfully rented the {car?.brand+" "+car?.model} vehicle</p>
-            <button className='btnAnim fs-4 my-5 backBtn' onClick={()=>{setPage("home")}}><img src=''/> GO BACK</button>
+            <button className='btnAnim fs-4 my-5 backBtn' onClick={()=>{setPage("home")}}> GO BACK</button>
         </section>
     )
 }
